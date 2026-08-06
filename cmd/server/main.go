98357228c628761bd2f7b25d3302b6b31b9d1edb
LOBERY <mixailov1234555@gmail.com>
2026-08-06 21:29:41 +0300

    upd

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"url_shortener_v3/internal/config"
+	"url_shortener_v3/internal/database"
+	"url_shortener_v3/internal/handler"
+	"url_shortener_v3/internal/repository"
+	"url_shortener_v3/internal/routes"
+	"url_shortener_v3/internal/service"
+)
 
 func main() {
-	fmt.Println("OK")
+
+	cfg := config.LoadConfig()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	conn, err := database.ConnectPostgres(ctx, cfg.DSN)
+	if err != nil {
+		return
+	}
+
+	repo := repository.NewRepository(conn)
+
+	srv := service.NewService(repo)
+
+	handl := handler.NewHandlerManager(srv)
+
+	routes.InitRoutes(routes.Handlers{Hm: handl}, "web")
 }
